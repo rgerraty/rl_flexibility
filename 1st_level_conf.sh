2356cbaf1e20aca8c44e8e495fa63b170a9959cf
rgerraty <rgerraty@gmail.com>
2016-03-02 11:28:56 -0500

    bash script for running confound regression on preprocessed data
    need to have generated confound file for this to work

--- 1st_level_conf.sh
+++ 1st_level_conf.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
 ####CODE for running 1st level confound analysis, need to have successfully run initial preprocessing and created confound regressors to run
-if [ -z $1 ]; then
-echo Script for running 1st level confound regression\. Make sure to enter which confound files to include in analysis'\n'Usage\:'\n'/home/rgerraty/1st_level_conf.sh 36par+spikes.txt
+if [ -z $2 ]; then
+echo Script for running 1st level confound regression\. 
+Make sure to enter input volumes as well as which confound files to include in analysis'\n'Usage\:
+'\n'~/GitHub/rl_flexibility/1st_level_conf.sh filtered_func_data.nii.gz 36par+spikes.txt
 
 else
 #paths to feat directories containing preprocessed data
-paths=/data/engine/kfoerde/FoodChoice/restingstate/*/*/*_237vol_120s_5mm_wm.feat/
 
-for i in $paths
-	do
-	#for each preprocessing directory check for filtered data, get TR and number of volumes, create and run .fsf file from template 
-	if [ -e $i$1 ];then 
-	conf=$i$1
-	out=$i../`basename $1 .txt`
-	filtdata=`ls $i\filtered_func_data.nii.gz`
+
+#check for filtered data, get TR and number of volumes, create and run .fsf file from template 
+if [ -e $1 ];
+	then 
+	conf=$(readlink -e $2)
+	filtdata=$(readlink -e $1)
+	out=$(dirname $filtdata/../$(basename $1 .txt))
+	
 	TR=`fslinfo $filtdata | grep pixdim4 | awk '{ print $2}'`
 	#get TR
 	vols=`fslinfo $filtdata | grep ^dim4 | awk '{ print $2}'`
 	#get number of volumes
 	#replace dummy lines in template fsf to make subject-specific temp fsf file
-	sed -e 's:XXOUTPUTXX:'$out':g' -e 's:XXTRXX:'$TR':g' -e 's:XXVOLSXX:'$vols':g' -e 's:XX4DDATAXX:'$filtdata':g' -e 's:XXCONFXX:'$conf':g'</home/rgerraty/scripts/fsf_files/conf_reg_design.fsf>tmp.fsf
+	sed -e 's:XXOUTPUTXX:'$out':g' -e 's:XXTRXX:'$TR':g' -e 's:XXVOLSXX:'$vols':g' -e 's:XX4DDATAXX:'$filtdata':g' -e 's:XXCONFXX:'$conf':g'<~/GitHub/rl_flexibility/conf_reg_design.fsf>tmp.fsf
 	feat tmp.fsf #run temp file
 	rm -rf tmp.fsf
 
-	else echo -e $1 does not exist in directory $i.'\n'moving on to next run
+	else echo -e $1 does not exist.
 	fi
-done
 
 fi
