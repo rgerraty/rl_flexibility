d4253f5613495c7173f87ebb6559b1719df0aa7d
rgerraty <rgerraty@gmail.com>
2016-03-12 16:33:22 -0500

    coherence across multiple pairs of time-series

--- mul_coher.m
+++ mul_coher.m
@@ -5,7 +5,7 @@ coh_mat=zeros(size(tss,2));
 
 for f=1:size(tss,2)
 	for g=h:size(tss,2)
-		[coh,freq]=mscohere(tss(:,f),tss(:,g),[],[],128,Fs);
+		[coh,freq]=mscohere(tss(:,f),tss(:,g),[],[],[],Fs);
 		coh_mat(f,g)=mean(coh(freq<=maxhz & freq>=minhz));
 	end
 	h=h+1;
