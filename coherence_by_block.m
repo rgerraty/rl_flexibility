9dd2a5c2ff2edaf26990d71657ee75052c78eef6
rgerraty <rgerraty@gmail.com>
2016-03-12 17:29:22 -0500

    get coherence for multiple blocks
    uses multiple_coher to get pairwise coherence for time-series

--- coherence_by_block.m
+++ coherence_by_block.m
@@ -1,27 +1,20 @@
-block_size=25;%size of each block for network analyses
-Fs=.5;
+function conn_cells=coherence_by_block(filename,block_size,Fs,minhz,maxhz)
 
-base='/data/engine/rgerraty/learn_dyncon/'
-rois='/all_HO_rois.txt';
+%numbers for flexibility paper
+%block_size=25;%size of each block for network analyses
+%Fs=.5;
+%minhz=.06
+%maxhz=.12
 
-subs=dir(strcat(base,'4*'));
 
-minhz=.06
-maxhz=.12
+tss=dlmread(filename);  
+ts_length=size(tss,1);
+k=1;
+for i=1:size(tss,1)/block_size
+	conn_mat(:,:,i)=...
+	mul_coher(tss(k:k+block_size-1,:),Fs,minhz,maxhz);
+	k=k+block_size;
+end
 
-for sub=1:size(subs,1)
-	sub
-	learn_runs=dir(strcat(base,subs(sub).name,'/','Learn*'));
-	num_runs=size(learn_runs,1);
-	for run=1:num_runs
-		tss=dlmread(strcat(base,subs(sub).name,'/',learn_runs(run).name,rois));  
-		ts_length=size(tss,1);
-		k=1;
-		for i=1:size(tss,1)/block_size
-			conn_mat(:,:,i+(run-1)*size(tss,1)/block_size)=...
-			mul_coher(tss(k:k+block_size-1,:),Fs,minhz,maxhz);
-			k=k+block_size;
-		end
-	end
-	conn_cells{sub}=mat2cell(conn_mat,size(tss,2),size(tss,2),[ones(1,size(conn_mat,3))]);
+conn_cells=mat2cell(conn_mat,size(tss,2),size(tss,2),[ones(1,size(conn_mat,3))]);
 end
