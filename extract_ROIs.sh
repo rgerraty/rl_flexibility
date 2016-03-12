069393fb2b98dc71b2f0a217c3ccfd0faad7c304
rgerraty <rgerraty@gmail.com>
2016-03-11 22:12:09 -0500

    extract folder of ROIs from 4D data

--- extract_ROIs.sh
+++ extract_ROIs.sh
@@ -29,6 +29,6 @@ else
 		do
 			fslmeants -i $fourd_data -o $ts_dir/$(basename $r .nii.gz).txt -m $r --eig
 		done
-		paste $ts_dir/*txt>$tsdir/all_rois.txt
+		paste $ts_dir/*txt>$ts_dir/all_rois.txt
 	fi
 fi
