788eec528a5e19a4171f8a42d2a5adb651cd00f2
rgerraty <rgerraty@gmail.com>
2016-04-05 15:58:28 -0400

    Grid search for optimizing multilayer CD parameters

--- Q_grid.m
+++ Q_grid.m
@@ -8,8 +8,8 @@ q_null=zeros(size(res_pars,1),size(omeg_pars,1),nsim);
 			for res = res_pars
 			o=1;
   			for omeg = omeg_pars
-    			[~,q_tmp(r,o,s)]=multiord_res_norm(corr_cells{sub},omeg, res);
-    			[~,q_null(r,o,s)]=multiord_res_norm_temporal(corr_cells{sub},omeg, res);
+    			[~,q_tmp(r,o,s)]=multiord_res_norm(conn_cell,omeg, res);
+    			[~,q_null(r,o,s)]=multiord_res_norm_temporal(conn_cell,omeg, res);
     			o=o+1;
   			end
   			r=r+1;
